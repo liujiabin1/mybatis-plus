a66c419bf619803b8df08fd78cfecf66fb71700c
zrb <liuailan>
2022-01-06 11:48:11 +0800

    自动填充数据库字段

---
+++ src/main/java/com/riverstar/handler/MyMetaObjectHandler.java
@@ -0,0 +1,21 @@
+package com.riverstar.handler;
+
+import com.baomidou.mybatisplus.core.handlers.MetaObjectHandler;
+import com.riverstar.tool.DateTool;
+import org.apache.ibatis.reflection.MetaObject;
+import org.springframework.stereotype.Component;
+
+@Component
+public class MyMetaObjectHandler implements MetaObjectHandler {
+
+    @Override
+    public void insertFill(MetaObject metaObject) {
+        this.strictInsertFill(metaObject,"createTime",Integer.class,DateTool.currentTime());
+        this.strictInsertFill(metaObject,"updateTime",Integer.class,DateTool.currentTime());
+    }
+
+    @Override
+    public void updateFill(MetaObject metaObject) {
+        this.strictUpdateFill(metaObject,"updateTime",Integer.class,DateTool.currentTime());
+    }
+}
